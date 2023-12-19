485acfafe35e98fc3790534466a71c11e549c1e2
akashgamage <bakashgamage@gmail.com>
2023-12-20 01:31:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+// Select The Elements
+var toggle_btn;
+var big_wrapper;
+var hamburger_menu;
+
+function declare() {
+  toggle_btn = document.querySelector(".toggle-btn");
+  big_wrapper = document.querySelector(".big-wrapper");
+  hamburger_menu = document.querySelector(".hamburger-menu");
+}
+
+const main = document.querySelector("main");
+
+declare();
+
+
+function toggleAnimation() {
+  
+  clone.addEventListener("animationend", () => {
+    document.body.classList.remove("stop-scrolling");
+    big_wrapper.remove();
+    clone.classList.remove("copy");
+    // Reset Variables
+    declare();
+    events();
+  });
+
+}
+
+function events() {
+  toggle_btn.addEventListener("click", toggleAnimation);
+  hamburger_menu.addEventListener("click", () => {
+    big_wrapper.classList.toggle("active");
+  });
+}
+
+events();
+
+// JavaScript for scroll animation
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function addAnimationClass() {
+  const content1 = document.querySelector('.content1');
+  const content2 = document.querySelector('.content2');
+
+  window.addEventListener('scroll', function() {
+    if (isElementInViewport(content1)) {
+      content1.classList.add('animate');
+    }
+    if (isElementInViewport(content2)) {
+      content2.classList.add('animate');
+    }
+  });
+}
+
+addAnimationClass();
